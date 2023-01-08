e0e3b3521e231bad67e9b81af9403147738cea31
Wafa Albur <wafaalbur21@gmail.com>
2023-01-08 14:44:00 +0300

    Deploy frontend to S3

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,2 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+# aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 cp --recursive --acl public-read /Users/Bandeto/reactnd-contacts-complete/build s3://wafasbucket/
